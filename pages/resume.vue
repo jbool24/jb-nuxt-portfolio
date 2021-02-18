532cc30000328cbefeff14a678bfbdd2cb448a24
Justin Bellero <j.bellero@dreamrocksmarttech.com>
2021-02-18 14:03:18 -0500

    [UPDATE]: fix resume page layout

--- pages/resume.vue
+++ pages/resume.vue
@@ -1,16 +1,6 @@
 <template lang="pug">
   main#content
     div(class="flex mt-20 mr-8 h-full w-full")
-        div(class="w-screen mx-auto")
+        div(class="w-screen md:w-2/3 mx-auto rounded overflow-hidden")
             iframe(id="document-iframe" src="https://drive.google.com/file/d/1FyFq5uACPDddqOZWNrmp95z-IHR5bxzC/preview" width="100%" height="1000")
 </template>
-
-<script></script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-</style>
