6de32d1f7ddf82deab1d80c55e301de6f7e36fc4
Justin Bellero <j.bellero@dreamrocksmarttech.com>
2021-02-18 13:03:43 -0500

    [UPDATE]: add user tip to portfolio page

--- pages/portfolio/_portfolio.vue
+++ pages/portfolio/_portfolio.vue
@@ -1,5 +1,7 @@
 <template lang="pug">
     div
+        div(class="absolute top-0 right-0 w-full md:w-4/6 lg:w-auto text-center animate__animated animate__fadeOutDown animate__delay-5s") 
+          div(class="mt-2 md:mt-16 md:mr-16 px-2 py-1 text-gray-200 bg-green-500 rounded animate__animated animate__heartBeat") Scroll, Tap L/R edges, or use L/R keys to navigate!
         component(:is="slideshow", :onEndExit="goTo")
 </template>
 
