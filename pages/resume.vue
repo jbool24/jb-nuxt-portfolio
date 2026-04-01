c16e9a284102cfedd1a07045cd9f9aa2a9005b22
Justin Bellero <jpbellero@gmail.com>
2026-04-01 09:41:57 -0400

    Update iframe source in resume.vue
    Publish updated resume March 2026

--- pages/resume.vue
+++ pages/resume.vue
@@ -2,5 +2,5 @@
   main#content
     div(class="flex mt-20 mr-8 h-full w-full")
         div(class="w-screen md:w-2/3 mx-auto rounded overflow-hidden")
-            iframe(id="document-iframe" src="https://drive.google.com/file/d/1FyFq5uACPDddqOZWNrmp95z-IHR5bxzC/preview" width="100%" height="1000")
+            iframe(id="document-iframe" src="https://drive.google.com/file/d/1r5PhdbtvobpBYs316JfvWKAkh3ii5mDu/preview" width="100%" height="1000")
 </template>
